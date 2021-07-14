7552ae624f14ce8130350fa860dca13813dff5a1
Gil Boymel <>
2021-07-14 14:27:12 +0300

    PMOB-2623

--- Framework/Sources/FloatingPanelController.swift
+++ Framework/Sources/FloatingPanelController.swift
@@ -307,7 +307,7 @@ open class FloatingPanelController: UIViewController, UIScrollViewDelegate, UIGe
 //            log.warning("Already added to a parent(\(parent))")
             return
         }
-        precondition((parent is UINavigationController) == false, "UINavigationController displays only one child view controller at a time.")
+        //precondition((parent is UINavigationController) == false, "UINavigationController displays only one child view controller at a time.")
         precondition((parent is UITabBarController) == false, "UITabBarController displays child view controllers with a radio-style selection interface")
         precondition((parent is UISplitViewController) == false, "UISplitViewController manages two child view controllers in a master-detail interface")
         precondition((parent is UITableViewController) == false, "UITableViewController should not be the parent because the view is a table view so that a floating panel doens't work well")
